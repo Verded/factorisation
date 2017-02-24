73148a30d30ce72c5b6d2dd1d6f32ac3449d8d0f
Yuinshiel <wee.jiawei.kevan@dhs.sg>
2017-02-24 15:14:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ $(function () {
 	});
 
 function appendText() {
-    var txt1 = "<p>Text.</p>";               // Create element with HTML  
-    var txt2 = $("<p></p>").text("Text.");   // Create with jQuery
+    var squareblue = "<p>Text.</p>";               // Create element with HTML  
     var txt3 = document.createElement("p");  // Create with DOM
     txt3.innerHTML = "Text.";
     $("body").append(txt1, txt2, txt3);      // Append the new elements 
 }
+
+$("squareblue").css("width": "210px", "height": "210px", "background": "blue", "border-width": "1px", "border-style" : "solid");
